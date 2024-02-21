2a72c1cc2c8716640fd312027b57e6efe57b0894
terremoth <dutra.astro@gmail.com>
2024-02-20 23:27:03 -0300

    Fixes for Codacy code quality: avoid XSS at any cost

--- demos/drawing.php
+++ demos/drawing.php
@@ -10,36 +10,36 @@ $doubleBorder = new DoubleLineBorder();
 
 try {
     $rectangle = new Rectangle(1, 1);
-    echo $rectangle->draw() . PHP_EOL;
+    echo htmlspecialchars($rectangle->draw()) . PHP_EOL;
 
     $rectangle = new Rectangle(2, 2);
-    echo $rectangle->draw() . PHP_EOL;
+    echo htmlspecialchars($rectangle->draw()) . PHP_EOL;
 
     $rectangle = new Rectangle(3, 3);
     echo $rectangle->draw($doubleBorder) . PHP_EOL;
 
     $rectangle = new Rectangle(2, 6);
-    echo $rectangle->draw() . PHP_EOL;
+    echo htmlspecialchars($rectangle->draw()) . PHP_EOL;
 
     $rectangle = new Rectangle(3, 15);
-    echo $rectangle->draw() . PHP_EOL;
+    echo htmlspecialchars($rectangle->draw()) . PHP_EOL;
 
     $rectangle = new Rectangle(1, 10);
-    echo $rectangle->draw() . PHP_EOL;
+    echo htmlspecialchars($rectangle->draw()) . PHP_EOL;
 
     $rectangle = new Rectangle(10, 1);
-    echo $rectangle->draw() . PHP_EOL;
+    echo htmlspecialchars($rectangle->draw()) . PHP_EOL;
 
     $rectangle = new Rectangle(4, 3);
-    echo $rectangle->draw() . PHP_EOL;
+    echo htmlspecialchars($rectangle->draw()) . PHP_EOL;
 
     $rectangle = new Rectangle(8, 3);
     $rectangle->setContentInsideBox('John Doe Lorem Ipsum Dolor Sit Amet');
-    echo $rectangle->draw() . PHP_EOL;
+    echo htmlspecialchars($rectangle->draw()) . PHP_EOL;
 
     $rectangle = new Rectangle(2, 16);
     $rectangle->setContentInsideBox('by terremoth and friends');
-    echo $rectangle->draw($doubleBorder) . PHP_EOL;
+    echo htmlspecialchars($rectangle->draw($doubleBorder)) . PHP_EOL;
 } catch (SideLessThanOneException $e) {
-    echo $e->getMessage();
+    echo htmlspecialchars($e->getMessage());
 }
